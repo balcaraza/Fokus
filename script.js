f6b116afc38c10ddd96a7f6c5cb04144616c93f0
Balam <balcaraza@gmail.com>
2025-08-27 17:58:36 -0600

    Feature | cambio de imagenes con funciones

--- script.js
+++ script.js
@@ -2,14 +2,21 @@ const html = document.querySelector('html');
 const botonCorto = document.querySelector('.app__card-button--corto');
 const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonLargo = document.querySelector('.app__card-button--largo');
+const banner = document.querySelector('.app__image');
 
 botonCorto.addEventListener('click', ()=>{
-    html.setAttribute('data-contexto', 'descanso-corto')
+    cambiarContexto('descanso-corto')
+
 })
 
 botonEnfoque.addEventListener('click', ()=>{
-    html.setAttribute('data-contexto', 'enfoque')
+    cambiarContexto('enfoque')
 })
 botonLargo.addEventListener('click', ()=>{
-    html.setAttribute('data-contexto', 'descanso-largo')
+    cambiarContexto('descanso-largo')
 })
+
+function cambiarContexto(contexto){
+    html.setAttribute('data-contexto', contexto);
+    banner.setAttribute('src', `./imagenes/${contexto}.png`);
+}
