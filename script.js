2decb3478b4677b15b59855fba04f953fbf352df
Balam <balcaraza@gmail.com>
2025-08-27 22:48:57 -0600

    Feature | se agregan textos dinamicos

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const botonCorto = document.querySelector('.app__card-button--corto');
 const botonEnfoque = document.querySelector('.app__card-button--enfoque');
 const botonLargo = document.querySelector('.app__card-button--largo');
 const banner = document.querySelector('.app__image');
+const titulo = document.querySelector('.app__title')
 
 botonCorto.addEventListener('click', ()=>{
     cambiarContexto('descanso-corto')
@@ -19,4 +20,23 @@ botonLargo.addEventListener('click', ()=>{
 function cambiarContexto(contexto){
     html.setAttribute('data-contexto', contexto);
     banner.setAttribute('src', `./imagenes/${contexto}.png`);
+
+    switch (contexto) {
+        case "enfoque":
+            titulo.innerHTML = `Optimiza tu productividad,<br>
+                <strong class="app__title-strong">sumérgete en lo que importa.`
+            break;
+        case "descanso-corto":
+            titulo.innerHTML = `
+            ¿Qué tal tomar un respiro?
+            <strong class="app__title-strong"> ¡Haz una pausa corta!</strong>`
+            break;
+        case "descanso-largo":
+            titulo.innerHTML = `
+                Hora de volver a la superficie
+                <strong class="app__title-strong">Haz una pausa larga.</strong>`
+                break;
+        default:
+            break;
+    }
 }
